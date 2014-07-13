361bf79ebb74a647dbdde7933d96f079c41ac7b4
tiensonqin <tiansheng.qin@upai.com>
2014-07-13 13:36:21 +0800

    add test

--- test/clj_netty/core_test.clj
+++ test/clj_netty/core_test.clj
@@ -1,21 +1,18 @@
 (ns clj-netty.core-test
   (:refer-clojure :exclude [sync])
   (:require [clojure.test :refer :all]
-            [clj-netty.core :refer :all]
-            ;; [com.netflix.hystrix.core :refer [defcommand]]
-            ))
+            [clj-netty.core :refer :all]))
 
-;; (defcommand divide
-;;   {:hystrix/fallback-fn (fn [service method args]
-;;                           (prn service method args))}
-;;   [service method args]
-;;   (apply / args))
+(defn divide
+  [service method args]
+  (try
+    (apply / args)
+    (catch Exception e
+      (prn (.getMessage e)))))
 
-;; (future (start-server 5555 divide))
-
-;; (def client (listen "localhost" 5555))
-
-;; (deftest hystrix
-;;   (are [x y] (= x y)
-;;        5 (sync client "test" "divide" [10 2])
-;;        nil (sync client "test" "divide" [10 0])))
+(deftest hystrix
+  (let [_ (future (start-server 5555 divide))
+        client (listen "localhost" 5555)]
+    (are [x y] (= x y)
+         5 (sync client "test" "divide" [10 2])
+         nil (sync client "test" "divide" [10 0]))))
